e5924049a35f8400b1d77bb21d5c557e6da14e82
moniesco22 <122181156+moniesco22@users.noreply.github.com>
2023-11-04 18:31:03 -0400

    taipy name submit

--- main.py
+++ main.py
@@ -10,10 +10,27 @@ message_data_node_cfg = Config.configure_data_node(id="message")
 build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
 scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
 
-page = """
+#page = """
 # Hello World 🌍 with *Taipy*This is my first Taipy test app.
-And it is running fine!
+#And it is running fine!
+#"""
+page = """
+Name: <|{input_name}|input|>
+<|submit|button|on_action=submit_scenario|>
+
+Message: <|{message}|text|>
 """
+
+input_name = "Taipy"
+message = None
+
+
+def submit_scenario(state):
+    state.scenario.input_name.write(state.input_name)
+    state.scenario.submit(wait=True)
+    state.message = scenario.message.read()
+
+
 
 Gui(page).run(use_reloader=True)  # use_reloader=True if you are in development
 
