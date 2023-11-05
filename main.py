c939ee5665b1f645077b1e6fcbf907727d9e1cc6
moniesco22 <122181156+moniesco22@users.noreply.github.com>
2023-11-04 21:30:08 -0400

    login

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ input_name_data_node_cfg = Config.configure_data_node(id="input_name")
 message_data_node_cfg = Config.configure_data_node(id="message")
 build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
 scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
+from taipy.gui import Markdown
 
-#page = """
-# Hello World 🌍 with *Taipy*This is my first Taipy test app.
-#And it is running fine!
-#"""
-page = """
-Name: <|{input_name}|input|>
+page_1 = """
+<h3 class="h5">ManagMed</h3>
+<|{content}|image|label=|on_action=function_name|>
+
+Username: <|{input_name}|input|>
+
+Password: <|{input_password}|input|>
+
 <|submit|button|on_action=submit_scenario|>
 
 Message: <|{message}|text|>
 """
 
-input_name = "Taipy"
-message = None
+page_2 = """
+<h3>testing something</h3>
+
+"""
+#content = 
+input_name = ""
+input_password = ""
+#message = ""
 
 
 def submit_scenario(state):
@@ -32,8 +41,8 @@ def submit_scenario(state):
 
 
 
-Gui(page).run(use_reloader=True)  # use_reloader=True if you are in development
-
+Gui(page_1).run(use_reloader=True)  # use_reloader=True if you are in development
+Gui(page_2).run(use_reloader=True)
 
 if __name__ == "__main__":
     Core().run()
