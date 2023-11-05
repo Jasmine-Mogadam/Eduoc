ab121ddc0dfbb17b4118ac5782bef65680ace4de
Jeffrey Edwards <98482948+instructcode@users.noreply.github.com>
2023-11-04 23:51:51 -0400

    Update AImodule.py

--- AImodule.py
+++ AImodule.py
@@ -1,130 +1,74 @@
-import openai
-import os
-
-import sys
-import chromadb
-
-from pyPDF2 import PdfReader
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import CohereEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
-
-#requirements
-#!pip install langchain
-#!pip install openai
-#!pip install PyPDF2
-#pip install faiss-cpu
-
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
-
-openai.api_key  = os.getenv('OPENAI_API_KEY')
-
-
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
-#     print(str(response.choices[0].message))
-    return response.choices[0].message["content"]
-
-messages =  [  
-{'role':'system', 'content':'You are a Teacher Assistant for a Computer Science class that evaluates and grades C++ Homework assignments turned in by students.'},    
-{'role':'user', 'content':'tell me a joke'},   
-  ]
-
-"""
-Prompting Principles¶
-Principle 1: Write clear and specific instructions
-Principle 2: Give the model time to “think”
-Tactics
-Tactic 1: Use delimiters to clearly indicate distinct parts of the input
-Delimiters can be anything like: ```, "".", < >, <tag> </tag>, :
-"""
-
-text = f"""
-You should express what you want a model to do by \ 
-providing instructions that are as clear and \ 
-specific as you can possibly make them. \ 
-This will guide the model towards the desired output, \ 
-and reduce the chances of receiving irrelevant \ 
-or incorrect responses. Don't confuse writing a \ 
-clear prompt with writing a short prompt. \ 
-In many cases, longer prompts provide more clarity \ 
-and context for the model, which can lead to \ 
-more detailed and relevant outputs.
-"""
-prompt = f"""
-Summarize the text delimited by triple backticks \ 
-into a single sentence.
-```{text}```
-"""
-response = get_completion_from_messages(messages, temperature=0)
-print(response)
-
-
-
-
-
-"""
-You can either set it as the OPENAI_API_KEY environment variable before using the library:
-
- !export OPENAI_API_KEY='sk-...'
-Or, set openai.api_key to its value:
-
-import openai
-openai.api_key = "sk-..."
-A note about the backslash
-In the course, we are using a backslash \ to make the text fit on the screen without inserting newline '\n' characters.
-GPT-3 isn't really affected whether you insert newline characters or not. But when working with LLMs in general, you may consider whether newline characters in your prompt may affect the model's performance.
-"""
-
-
-
-Load Python libraries to view HTML
-from IPython.display import display, HTML
-display(HTML(response))
-
-
-
-
-
-
-
 import constants
-from langchain.document_loaders import TextLoader
-#from langchain.document_loaders import DirectoryLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+import cohereconstant
+import cohere
+#from langchain.prompts import PromptTemplate
+#from langchain.chains import Cohere
+#from langchain.chains import OpenAI
 
-os.environ["OPENAI_API_KEY"] = constants.APIKEY
+from langchain.chains.question_answering import load_qa_chain
+#from langchain.
+from langchain.llms import Cohere
 
-#https://github.com/jerryjliu/llama_index
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+#code partially sourced from: https://www.youtube.com/watch?v=TLf90ipMzfE
+import os
 
-#https://www.youtube.com/watch?v=9AXP7tCI9PI
-
-
-query = sys.argv[1]
-
+osTrackCohere = True
 
 
-
-loader = TextLoader('data.txt')
-#for a directory instead of file
-#loader = DirectoryLoader(".", glob="*.txt")
-index = VectorstoreIndexCreator().from_loaders([loader])
+if not osTrackCohere:
+    os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 
-print(index.query(query, llm=ChatOpenAI()))
+#create rerouter, if PDF do this, otherwise use text reader
+reader = PdfReader("C:\\Users\\FBITinfoilprotection\\Desktop\\chatgptexperiments\\langchain\\JeffreyEdwards.pdf")
+
+raw_text = ''
+for i, page in enumerate(reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+
+raw_text[:100]
+# potential problem is if pdf is decoded
+
+
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap = 200,
+    length_function = len,
+)
+texts = text_splitter.split_text(raw_text)
+
+if(osTrackCohere):
+    embeddings = CohereEmbeddings(cohere_api_key=cohereconstant.APIKEY)
+else:
+    embeddings = OpenAIEmbeddings()
+    
+#takes text chunks and finds corresponding embeddings
+docsearch = FAISS.from_texts(texts, embeddings)
 
 
 
 
 
+chain = load_qa_chain(Cohere(cohere_api_key=cohereconstant.APIKEY), chain_type="stuff")
+
+query = "who is the person featured in this document?"
+docs = docsearch.similarity_search(query)
+print(chain.run(input_documents=docs, question=query))
+
+
+
+
+
+#read documentation, put it on top of mind before coding
 
 
 def load_pdf() 
